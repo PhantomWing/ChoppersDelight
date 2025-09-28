d1d55c556cc4b4c8a5e5c64b632346aa46dd8a00
Calvin Poleij <calvin@ichicraft.com>
2025-09-28 19:12:07 +0200

    Add empty Fabric project

---
+++ src/client/java/com/phantomwing/choppersdelight/ChoppersdelightClient.java
@@ -0,0 +1,10 @@
+package com.phantomwing.choppersdelight;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChoppersdelightClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
--- src/main/java/com/phantomwing/choppersdelight/ChoppersDelight.java
+++ src/main/java/com/phantomwing/choppersdelight/ChoppersDelight.java
@@ -1,109 +1,24 @@
 package com.phantomwing.choppersdelight;
 
-import com.phantomwing.choppersdelight.component.ModDataComponents;
-import com.phantomwing.choppersdelight.recipe.ModRecipes;
-import com.phantomwing.choppersdelight.renderer.DecoratedCuttingBoardItemStackRenderer;
-import com.phantomwing.choppersdelight.renderer.DecoratedCuttingBoardRenderer;
-import com.phantomwing.choppersdelight.block.ModBlockEntityTypes;
-import com.phantomwing.choppersdelight.block.ModBlocks;
-import com.phantomwing.choppersdelight.item.ModItems;
-import com.phantomwing.choppersdelight.ui.ModCreativeModeTab;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.loading.FMLEnvironment;
-import net.neoforged.neoforge.client.event.EntityRenderersEvent;
-import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
-import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-import net.neoforged.neoforge.client.gui.ConfigurationScreen;
-import org.jetbrains.annotations.NotNull;
+import net.fabricmc.api.ModInitializer;
+
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.mojang.logging.LogUtils;
+public class Choppersdelight implements ModInitializer {
+	public static final String MOD_ID = "choppersdelight";
 
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-@Mod(ChoppersDelight.MOD_ID)
-public class ChoppersDelight {
-    public static final String MOD_ID = "choppersdelight";
-    public static final Logger LOGGER = LogUtils.getLogger();
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
 
-
-
-    public ChoppersDelight(IEventBus eventBus, ModContainer modContainer) {
-        eventBus.addListener(this::commonSetup);
-
-        modContainer.registerConfig(ModConfig.Type.COMMON, Configuration.COMMON_CONFIG);
-
-        // This will use NeoForge's ConfigurationScreen to display this mod's configs (Client only)
-        if (FMLEnvironment.dist.isClient()) {
-            modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
-        }
-
-        NeoForge.EVENT_BUS.register(this);
-
-        Compatibility.checkInstalledMods();
-        registerManagers(eventBus);
-    }
-
-
-
-    // Register all managers to the event bus.
-    private void registerManagers(IEventBus eventBus) {
-        ModItems.register(eventBus);
-        ModBlocks.register(eventBus);
-        ModBlockEntityTypes.register(eventBus);
-        ModCreativeModeTab.register(eventBus);
-        ModDataComponents.register(eventBus);
-        ModRecipes.register(eventBus);
-    }
-
-    private void commonSetup(FMLCommonSetupEvent event) {
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-
-    }
-
-    @EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-        }
-
-        @SubscribeEvent
-        public static void onRegisterRenderers(EntityRenderersEvent.RegisterRenderers event) {
-            event.registerBlockEntityRenderer(ModBlockEntityTypes.DECORATED_CUTTING_BOARD.get(), DecoratedCuttingBoardRenderer::new);
-        }
-
-        @SubscribeEvent
-        public static void onRegisterClientExtensions(RegisterClientExtensionsEvent event) {
-            event.registerItem(
-                    new IClientItemExtensions() {
-                        private final BlockEntityWithoutLevelRenderer renderer =
-                                new DecoratedCuttingBoardItemStackRenderer(
-                                        Minecraft.getInstance().getBlockEntityRenderDispatcher(),
-                                        Minecraft.getInstance().getEntityModels()
-                                );
-
-                        @Override
-                        public @NotNull BlockEntityWithoutLevelRenderer getCustomRenderer() {
-                            return renderer;
-                        }
-                    },
-                    ModItems.DECORATED_CUTTING_BOARD.get()
-            );
-        }
-    }
+		LOGGER.info("Hello Fabric world!");
+	}
 }
